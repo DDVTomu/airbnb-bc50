37a44f24c4da03f1fc8e71d1d967a6d55844b78a
DDVTomu <phan031099@gmail.com>
2023-09-27 04:47:12 +0700

    fix fetch data

--- src/lib/api.tsx
+++ src/lib/api.tsx
@@ -11,7 +11,8 @@ export async function fetchAPI(path: any) {
         "Content-Type": "application/json",
         tokenCybersoft: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA1MCIsIkhldEhhblN0cmluZyI6IjE4LzAxLzIwMjQiLCJIZXRIYW5UaW1lIjoiMTcwNTUzNjAwMDAwMCIsIm5iZiI6MTY3NzQzMDgwMCwiZXhwIjoxNzA1NjgzNjAwfQ.s4X0R0Wi80X0f9MLJ2XYxRKJdQJBW27dwvkpfN03100`,
       },
-    });
+      cache: 'no-store',
+    },);
     const data = await response.json();
     return data;
   } catch (error) {
